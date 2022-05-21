b97e88c235a3387713c6867b12ad53df71342ea6
Nico Franke <nico.franke01@gmail.com>
2022-05-21 15:17:30 +0200

    Update infos

--- usoptimize/cli.py
+++ usoptimize/cli.py
@@ -1,12 +1,14 @@
 import argparse
 import os
+import subprocess
 import sys
 
 from usoptimize.convert import convert
 
 
 def cli():
-    parser = argparse.ArgumentParser(description='Convert UltraStar Songs to mp4.')
+    parser = argparse.ArgumentParser(
+        description='A Python CLI UltraStar Song Converter to optimize song files for the web.')
     parser.add_argument('-i', '--input', metavar='<path of songs>', type=str, nargs=1, required=True,
                         help='Filepath of songs that should be converted')
     parser.add_argument('-o', '--output', metavar='<output path>', type=str, nargs=1, required=True,
@@ -21,6 +23,13 @@ def cli():
 
     if not os.path.isdir(input_path) or not os.path.isdir(output_path):
         print('The path specified does not exist')
+        sys.exit()
+
+    # check if ffmpeg is installed in path
+    try:
+        subprocess.run('ffmpeg -version', stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    except FileNotFoundError:
+        print('FFmpeg is not installed in path. Please install it and try again. (Check How to use https://pypi.org/project/usoptimize/')
         sys.exit()
 
     convert(input_path, output_path, args.replace)
